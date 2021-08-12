5fe7554cdcc5e3c2e8296c2026159b6427a3c57d
OJPalenzuela <palenzuelaomar@gmail.com>
2021-08-11 19:07:29 -0500

    Se empieza implementar el registro desde la api

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,40 +1,47 @@
-import React, {useState} from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
-import { DivAuth, Header, Button, Logo, Form, Label, DivLink, Input, ALink } from '../styles/styleAuth';
+import {
+    DivAuth,
+    Header,
+    Button,
+    Logo,
+    Form,
+    Label,
+    DivLink,
+    Input
+} from '../styles/styleAuth';
 
 const Register = () => {
 
-    const [data, setData] = useState({
-        form: {
-            nombre: "",
-            apellido:"",
-            email: "",
-            password: "",
-            verificar:""
-        }}
-    )
-    
-    const handleChange = e =>{
-        setData({
-            form: {
-            ...data.form,
+    const [credentials, setCredentials] = useState({
+        name: "",
+        lastname: "",
+        email: "",
+        password: ""
+    });
+
+    const handleChange = (e) => {
+        setCredentials({
+            ...credentials,
             [e.target.name]: e.target.value
-            }
         });
-    }
+    };
 
-    const handleSubmit = (e)=>{
+    const handleSubmit = (e) => {
         e.preventDefault();
-    }
+        register()
+      };
 
-    const iniciarSesion = (e) =>{
-        const {form} = data;
+      const register = async () => {
+        try {
+          const url = `http://localhost:5000/api/register`;
+          await axios.post(url, credentials);
 
-        if(form.verificar === form.password){
+        } catch (error) {
+          return error;
         }
-        
-
-    }
+      };
 
     return (
         <DivAuth>
@@ -45,35 +52,53 @@ const Register = () => {
             <Form action="" onSubmit={handleSubmit}>
                 <Label htmlFor="nombre">
                     Nombre
-                    <Input type="text" name="nombre" id="nombre" placeholder="Ingresa tu" />
+                    <Input
+                        onChange={handleChange}
+                        type="text"
+                        name="name"
+                        id="name"
+                        placeholder="Ingresa tu nombre" />
                 </Label>
+
                 <Label htmlFor="apellido">
                     Apellido
-                    <Input type="text" name="apellido" id="apellido" placeholder="Ingresa tu apellido" />
+                    <Input
+                        onChange={handleChange}
+                        type="text"
+                        name="lastaname"
+                        id="lastname"
+                        placeholder="Ingresa tu apellido" />
                 </Label>
-                
+
                 <Label htmlFor="inputEmail">
                     Correo electrónico
-                    <Input onChange={handleChange} type="email" name="email" id="inputEmail" placeholder="Ingrese su correo electrónico" />
+                    <Input
+                        onChange={handleChange}
+                        type="email"
+                        name="email"
+                        id="inputEmail"
+                        placeholder="Ingrese su correo electrónico" />
                 </Label>
 
                 <Label htmlFor="inputPassword">
                     Contraseña
-                    <Input onChange={handleChange} type="password" name="password" id="inputPassword" placeholder="Ingresa una contraseña" />
-                </Label>
-                <Label htmlFor="inputPasswordVerific">
-                    Verificar contraseña
-                    <Input onChange={handleChange} type="password" name="verificar" id="inputPasswordVerific" placeholder="Verificar contraseña" />
+                    <Input
+                        onChange={handleChange}
+                        type="password"
+                        name="password"
+                        id="inputPassword"
+                        placeholder="Ingresa una contraseña" />
                 </Label>
 
-                <Button type="submit"
-                    onClick={iniciarSesion}>
+                 
+
+                <Button type="submit">
                     Registrarme
                 </Button>
             </Form>
 
             <DivLink>
-                <p>¿Ya tienes una cuenta? <Link style={{color: "#26b67d" }} to="/login">Inicia sesión</Link></p>
+                <p>¿Ya tienes una cuenta? <Link style={{ color: "#26b67d" }} to="/login">Inicia sesión</Link></p>
             </DivLink>
 
         </DivAuth>
