b264bd47c8a4b0a8dcf6abd95256712a3e6fe33e
Miguel Alvarez <ing.migueljalvarez@gmail.com>
2021-08-12 09:03:18 -0500

    fix publicpath

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "build"),
     assetModuleFilename: "assets/svg/[name][ext][query]",
-    publicPath: "/",
+    publicPath: "./",
   },
   resolve: {
     extensions: [".js", ".jsx"],
